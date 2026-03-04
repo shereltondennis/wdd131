cf8fce8c18e99908e7f225e8354b56fc01e51ec7
Sherelton B. Dennis <shereltondennis@gmail.com>
2026-03-03 22:43:37 -0800

    ok

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,9 @@
+const yearSpan = document.querySelector("#currentyear");
+if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+}
+
+const lastModifiedParagraph = document.querySelector("#lastModified");
+if (lastModifiedParagraph) {
+    lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
+}
