0ec5e823dd013a516fdbb8dab824b47cde32050f
Sherelton B. Dennis <shereltondennis@gmail.com>
2026-03-06 23:00:50 -0800

    ok

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,5 +5,5 @@ if (yearSpan) {
 
 const lastModifiedParagraph = document.querySelector("#lastModified");
 if (lastModifiedParagraph) {
-    lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
+    lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
 }
