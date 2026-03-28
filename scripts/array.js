71cda9d3bc3e244a070ae58e8ddc3949adca8fbd
Sherelton B. Dennis <shereltondennis@gmail.com>
2026-03-27 22:48:27 -0700

    done

--- scripts/array.js
+++ scripts/array.js
@@ -5,63 +5,63 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "images/aba_nigeria-temple.jpg"
   },
   {
-    templeName: "Manti Utah",
-    location: "Manti, Utah, United States",
-    dedicated: "1888, May, 21",
+    templeName: "abidjan Ivory Coast",
+    location: "Abidjan, Ivory Coast",
+    dedicated: "2025, May, 25",
     area: 74792,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    "images/abidjan_ivory_temple.jpg"
   },
   {
-    templeName: "Payson Utah",
-    location: "Payson, Utah, United States",
-    dedicated: "2015, June, 7",
+    templeName: "Accra Ghana",
+    location: "Accra, Ghana",
+    dedicated: "2004, June, 11",
     area: 96630,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    "images/accra_ghana_temple.jpg"
   },
   {
-    templeName: "Yigo Guam",
-    location: "Yigo, Guam",
-    dedicated: "2020, May, 2",
+    templeName: "Adelaide Temple",
+    location: "Marden, South Australia",
+    dedicated: "2000, June, 15",
     area: 6861,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    "images/adelaiade_au_temple.jpg"
   },
   {
-    templeName: "Washington D.C.",
-    location: "Kensington, Maryland, United States",
-    dedicated: "1974, November, 19",
-    area: 156558,
+    templeName: "Bern Switzerland Temple",
+    location: "bern, Switzerland",
+    dedicated: "11–15 September 1955",
+    area: 35546,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    "images/bern_switzerland_temple.jpg"
   },
   {
-    templeName: "Lima Perú",
-    location: "Lima, Perú",
-    dedicated: "1986, January, 10",
-    area: 9600,
+    templeName: "Brisbane Australia Temple",
+    location: "Brisbane, Australia",
+    dedicated: " 15 June 2003",
+    area:  10700,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    "images/brisbane_australia_temple.jpg"
   },
   {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
+    templeName: "Lagos Nigeria Temple",
+    location: "Lagos, Nigeria",
+    dedicated: "2025, May, 10",
     area: 116642,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    "images/lagos_nigeria_temple.jpg"
   },
   {
-    templeName: "Salt Lake Utah",
-    location: "Salt Lake City, Utah, United States",
+    templeName: "Modesto California Temple",
+    location: "Modesto, California, United States",
     dedicated: "1893, April, 6",
     area: 382207,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-25762.jpg"
+    "images/modesto_california_temple.jpg"
   },
   {
     templeName: "Laie Hawaii",
@@ -69,16 +69,9 @@ const temples = [
     dedicated: "1919, November, 27",
     area: 47224,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple/laie-hawaii-temple-3840.jpg"
+    "images/laie_hawaii_temple.jpg"
   },
-  {
-    templeName: "Cardston Alberta",
-    location: "Cardston, Alberta, Canada",
-    dedicated: "1923, August, 26",
-    area: 81700,
-    imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/cardston-alberta-temple/cardston-alberta-temple-68769.jpg"
-  }
+  
 ];
 
 const templeCards = document.querySelector("#temple-cards");
